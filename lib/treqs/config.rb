a03e3f1867fc1a66ea601e23c215f45f73510cdd
Frank Kair <frankkair@gmail.com>
2021-12-31 16:08:03 -0300

    Adds default headers for content-type
    Co-authored-by: André H Martello <andre.martello@mercadolivre.com>

--- lib/treqs/config.rb
+++ lib/treqs/config.rb
@@ -3,13 +3,16 @@
 # Class containing the request information
 class Config
   attr_reader :method, :url, :headers, :body, :params
+  DEFAULT_HEADERS = { 'Content-Type' => 'application/json' }.freeze
 
   # @param [Hash]
   def initialize(config_hash)
     @method = config_hash['method']
     @url = URI(config_hash['url'])
-    @headers = config_hash['headers']
+    headers = config_hash['headers'] || {}
     @body = config_hash['body']
     @params = config_hash['params']
+
+    @headers = DEFAULT_HEADERS.merge(headers)
   end
 end
