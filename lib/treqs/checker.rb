6b5cc18624a9487caf32eccb8131c6b566bc51f8
André H Martello <andre.martello@mercadolivre.com>
2022-01-07 19:36:26 -0300

    Restructure validation calls

--- lib/treqs/checker.rb
+++ lib/treqs/checker.rb
@@ -19,13 +19,32 @@ module Checker
   # @return [Hash]
   def call(config_hash)
     config_hash
-      .keys
-      .to_set
-      .then { |keys_set| validate_required(keys_set) }
-      .then { |keys_set| validate_allowed(keys_set) }
+      .then { |config_hash| validate_structure(config_hash) }
+      .then { |config_hash| validate_values(config_hash) }
 
     config_hash
-      .then { |request_hash| validate_http_methods_allowed(request_hash) }
+  end
+
+  # @param [Hash]
+  # @raise [Checker::RequiredKeysError]
+  # @raise [Checker::ExtraKeysError]
+  # @return [Hash]
+  def self.validate_structure(config_hash)
+    config_hash
+      .keys
+      .to_set
+      .then { |keys_set| validate_required_keys(keys_set) }
+      .then { |keys_set| validate_allowed_keys(keys_set) }
+
+    config_hash
+  end
+
+  # @param [Hash]
+  # @raise [Checker::ExtraKeysError]
+  # @return [Hash]
+  def self.validate_values(config_hash)
+    config_hash
+      .then { |config_hash| validate_http_methods(config_hash) }
 
     config_hash
   end
@@ -33,7 +52,7 @@ module Checker
   # @param [Set]
   # @raise [Checker::RequiredKeysError]
   # @return [Set]
-  def self.validate_required(input_keys)
+  def self.validate_required_keys(input_keys)
     minimum = REQUIRED_KEYS.all?(input_keys)
     error_msg = "Missing #{REQUIRED_KEYS} in the config file"
     raise RequiredKeysError, error_msg unless minimum
@@ -44,7 +63,7 @@ module Checker
   # @param [Set]
   # @raise [Checker::ExtraKeysError]
   # @return [Set]
-  def self.validate_allowed(input_keys)
+  def self.validate_allowed_keys(input_keys)
     possible_keys = REQUIRED_KEYS + EXTRA_KEYS
     has_diff = input_keys.difference(possible_keys).count.positive?
     error_msg = "Can only use #{possible_keys}"
@@ -56,12 +75,12 @@ module Checker
   # @param [Hash]
   # @raise [Checker::ExtraKeysError]
   # @return [Hash]
-  def self.validate_http_methods_allowed(request_hash)
-    valid_method = HTTP_METHODS_ALLOWED.include?(request_hash["method"].downcase)
+  def self.validate_http_methods(config_hash)
+    valid_method = HTTP_METHODS_ALLOWED.include?(config_hash["method"].downcase)
     error_msg = "Can only use one of #{HTTP_METHODS_ALLOWED.to_a} as http method"
     raise UnsupportedHTTPMethod, error_msg if not valid_method
 
-    request_hash
+    config_hash
   end
 
   module_function :call
